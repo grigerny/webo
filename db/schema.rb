d4492056974c50459bc98fa39f37248623f0a0a0
Gary Riger <grigerny@aol.com>
2013-02-16 18:02:48 -0500

    update 0.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130216201140) do
+ActiveRecord::Schema.define(:version => 20130216220321) do
 
   create_table "categories", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
 end
