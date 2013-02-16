073b99e7a0a56d8ec360be4bf6457c6d557464ab
Gary Riger <grigerny@aol.com>
2013-02-16 15:17:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webo::Application.config.secret_token = '356e3d51582d5eb16d16264b5812b6e97914b4b802487dcceca0f490b35e3e5a122c50f1e1359b7c3d29ad3a73bf78ab14620e2ef852f05c93aa224dcd946593'
