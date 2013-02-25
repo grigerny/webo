444d732262d6fd553796f035849d4cc99145c3e9
Gary Riger <grigerny@aol.com>
2013-02-24 22:01:02 -0500

    update 0.9 unified search/add into 1 form

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,4 +4,7 @@ class Link < ActiveRecord::Base
   acts_as_voteable
   
   attr_accessible :name, :category_id, :user_id
+  validates_exclusion_of :name, :in => ["http://"]
+  validates_format_of :name, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  
 end
